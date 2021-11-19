be76b6efc9444de5e1bdb35e1332b12ecd920086
anisko13 <anisochka12341@gmail.com>
2021-11-19 18:45:53 +0200

    made task2

---
+++ HW_2.py
@@ -0,0 +1,16 @@
+class Buyer:
+    first_name = ''
+    last_name = ''
+    patronymic = ''
+    phone_number = ''
+
+    def __init__(self, first_name, last_name, patronymic, phone_number):
+        self.first_name, self.last_name, self.patronymic, self.phone_number = first_name, last_name, patronymic, phone_number
+
+    def __str__(self):
+        return f'Покупатель - {self.first_name} {self.last_name} {self.patronymic} {self.phone_number}'
+
+buyer1 = Buyer('Anna', 'Prokopchuk', 'Viktorovna', '000 000 00 00')
+buyer2 = Buyer('Youjin', 'Prokopchuk', 'Pavlovich', '000 000 00 00')
+
+print(buyer1,'\n', buyer2)
