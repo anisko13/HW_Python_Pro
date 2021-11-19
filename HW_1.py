8e1b03fa288881a7af35dad85371b384c8e96a37
anisko13 <anisochka12341@gmail.com>
2021-11-19 18:32:41 +0200

    made task1

---
+++ HW_1.py
@@ -0,0 +1,17 @@
+class Product:
+    price = ''
+    description = ''
+    size = ''
+
+    def __init__(self, price, description, size):
+        self.price = price
+        self.description = description
+        self.size = size
+
+    def __str__(self):
+        return f'Продукт - {self.description}'
+
+product1 = Product(5, 'fluffy', 6)
+product2 = Product(4, 'strict', 7)
+
+print(product1,'\n', product2)
