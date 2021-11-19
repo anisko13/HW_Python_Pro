0e8f6fb9625de1d6793fddb6f9a3c25abe11e959
anisko13 <anisochka12341@gmail.com>
2021-11-19 20:02:32 +0200

    made task2-3

--- HW2_2.py
+++ HW2_2.py
@@ -5,7 +5,13 @@ class Student(Human):
 
     def get_info(self):
         return f'Описание студента - {self.first_name} {self.last_name} {self.patronymic} {self.phone_number}'
+    def __str__(self):
+        return f'Описание студента - {self.first_name} {self.last_name} {self.patronymic} {self.phone_number}'
+
+
 
 student1 = Student('Yana', 'Prokopchuk', 'Viktorovna', '000 000 00 00')
 
-print(student1.get_info())
+
+if __name__ == '__main__':
+    print(student1.get_info())
---
+++ HW2_3.py
@@ -0,0 +1,37 @@
+from HW_Python_Pro.HW2_2 import Student, student1
+
+
+class Group:
+    students = []  # [1, 2, 3]
+                   # 0-1 1-2 2-3
+
+    def add(self, student: Student):
+        self.students.append(student)
+
+    def delete(self, index):
+        self.students.pop(index)
+
+    def get_student_by_last_name (self, last_name):
+        for student in self.students:
+            if last_name == student.last_name:
+                return student
+        return
+
+
+    def __str__(self):
+        result = ''
+        for student in self.students:
+            result += student.__str__() + '\n'
+        return result
+
+group1 = Group()
+group1.add(student1)
+group1.add(Student('ivan', 'ivanov', 'ivanovich', '000 000'))
+
+if __name__ == '__main__':
+    print(group1)
+    print(group1.get_student_by_last_name('Prokopchuk'))
+    print(group1.get_student_by_last_name('Dziuba'))
+    group1.delete(0)
+    print(group1.get_student_by_last_name('Prokopchuk'))
+
